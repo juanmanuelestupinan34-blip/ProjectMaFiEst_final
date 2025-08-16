a903c42acd71b7ef16778bec577308b0c4362fef
Juan-E-Medina <estupinanj89@gmail.com>
2025-08-16 17:25:37 -0500

    Create Inicio.jsx

---
+++ client/src/pages/Inicio.jsx
@@ -0,0 +1,23 @@
+import Menu from '../components/Menu'
+import Footer from '../components/Footer'
+
+const Inicio = () => {
+    return (
+        <div className="inicio-container">
+            <Menu />
+            <main className="inicio-main">
+                <h1>Bienvenido a MaFiEst</h1>
+                <p>
+                    En MaFiEst nos apasiona la enseñanza de las matemáticas. Ofrecemos clases pregrabadas de alta calidad y asesorías personalizadas para clases presenciales, adaptadas a las necesidades de cada estudiante. 
+                    Nuestro objetivo es que comprendas realmente las matemáticas, desarrolles pensamiento lógico y te prepares con éxito para tus retos académicos.
+                </p>
+                <p>
+                    Explora nuestras secciones para conocer más sobre nuestros servicios, misión, visión, y cómo podemos ayudarte a alcanzar tus metas educativas.
+                </p>
+            </main>
+
+        </div>
+    )
+}
+
+export default Inicio;
