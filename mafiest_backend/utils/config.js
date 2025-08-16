59c6916aeac3f10d7bd5002bdf5a6a951217aac1
Juan-E-Medina <estupinanj89@gmail.com>
2025-08-16 14:02:04 -0500

    Update config.js

--- mafiest_backend/utils/config.js
+++ mafiest_backend/utils/config.js
@@ -1,1 +1,22 @@
 
+require('dotenv').config()
+
+const PORT = process.env.PORT
+const SECRET = process.env.SECRET
+
+// Base de datos de usuarios
+const MONGO_URI_USERS = process.env.NODE_ENV === 'test'
+  ? process.env.TEST_MONGO_URI_USERS
+  : process.env.MONGO_URI_USERS
+
+// Base de datos de contactos
+const MONGO_URI_CONTACT = process.env.NODE_ENV === 'test'
+  ? process.env.TEST_MONGO_URI_CONTACT
+  : process.env.MONGO_URI_CONTACT
+
+module.exports = {
+  PORT,
+  SECRET,
+  MONGO_URI_USERS,
+  MONGO_URI_CONTACT
+}
