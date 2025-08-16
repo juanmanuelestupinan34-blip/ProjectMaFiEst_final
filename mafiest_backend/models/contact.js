6c3c009d76bcc0419f6a5ae07614692f9bf01f95
Juan-E-Medina <estupinanj89@gmail.com>
2025-08-16 12:48:56 -0500

    Update contact.js

--- mafiest_backend/models/contact.js
+++ mafiest_backend/models/contact.js
@@ -1,56 +1,57 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+const { contactConnection } = require("../utils/db");
 
 const contactSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'El nombre es obligatorio'],
-    minlength: [3, 'El nombre debe tener al menos 3 caracteres'],
-    trim: true
+    required: [true, "El nombre es obligatorio"],
+    minlength: [3, "El nombre debe tener al menos 3 caracteres"],
+    trim: true,
   },
   email: {
     type: String,
-    required: [true, 'El correo es obligatorio'],
-    minlength: [5, 'El correo debe tener al menos 5 caracteres'],
+    required: [true, "El correo es obligatorio"],
+    minlength: [5, "El correo debe tener al menos 5 caracteres"],
     trim: true,
     lowercase: true,
     match: [
       /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,})+$/,
-      'Por favor, proporciona un correo válido'
-    ]
+      "Por favor, proporciona un correo válido",
+    ],
   },
   phone: {
     type: String,
-    required: [true, 'El número de teléfono es obligatorio'],
+    required: [true, "El número de teléfono es obligatorio"],
     trim: true,
     match: [
       /^[0-9+\-()\s]*$/,
-      'El teléfono solo puede contener números y caracteres válidos (+, -, (, ))'
-    ]
+      "El teléfono solo puede contener números y caracteres válidos (+, -, (, ))",
+    ],
   },
   subject: {
     type: String,
     trim: true,
-    maxlength: [100, 'El asunto no puede tener más de 100 caracteres']
+    maxlength: [100, "El asunto no puede tener más de 100 caracteres"],
   },
   message: {
     type: String,
-    required: [true, 'El mensaje es obligatorio'],
-    minlength: [10, 'El mensaje debe tener al menos 10 caracteres'],
-    maxlength: [1000, 'El mensaje no puede superar los 1000 caracteres'],
-    trim: true
-  }
+    required: [true, "El mensaje es obligatorio"],
+    minlength: [10, "El mensaje debe tener al menos 10 caracteres"],
+    maxlength: [1000, "El mensaje no puede superar los 1000 caracteres"],
+    trim: true,
+  },
 }, {
-  timestamps: true
-})
+  timestamps: true,
+});
 
-contactSchema.set('toJSON', {
+contactSchema.set("toJSON", {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
 
-const Contact = mongoose.model('Contact', contactSchema)
+const Contact = contactConnection.model("Contact", contactSchema);
 
-module.exports = Contact
+module.exports = Contact;
