9efdc03e161ae238ae724671398d1c790ff06cda
Juan-E-Medina <estupinanj89@gmail.com>
2025-08-16 12:45:41 -0500

    Update user.js

--- mafiest_backend/models/user.js
+++ mafiest_backend/models/user.js
@@ -1,56 +1,56 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+const { userConnection } = require("../utils/db");
 
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
-    required: [true, 'El nombre de usuario es obligatorio'],
-    minlength: [3, 'El nombre de usuario debe tener al menos 3 caracteres'],
+    required: [true, "El nombre de usuario es obligatorio"],
+    minlength: [3, "El nombre de usuario debe tener al menos 3 caracteres"],
     unique: true,
-    trim: true
+    trim: true,
   },
   name: {
     type: String,
-    required: [true, 'El nombre completo es obligatorio'],
-    minlength: [3, 'El nombre debe tener al menos 3 caracteres'],
+    required: [true, "El nombre completo es obligatorio"],
+    minlength: [3, "El nombre debe tener al menos 3 caracteres"],
     unique: true,
-    trim: true
+    trim: true,
   },
   email: {
     type: String,
-    required: [true, 'El correo es obligatorio'],
-    minlength: [5, 'El correo debe tener al menos 5 caracteres'],
+    required: [true, "El correo es obligatorio"],
+    minlength: [5, "El correo debe tener al menos 5 caracteres"],
     unique: true,
     trim: true,
     lowercase: true,
     match: [
       /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,})+$/,
-      'Por favor, proporciona un correo válido'
-    ]
+      "Por favor, proporciona un correo válido",
+    ],
   },
   rol: {
     type: String,
-    required: [true, 'El rol es obligatorio'],
-    trim: true
+    required: [true, "El rol es obligatorio"],
+    trim: true,
   },
   passwordHash: {
     type: String,
-    required: [true, 'La contraseña es obligatoria'],
-    minlength: [8, 'La contraseña debe tener al menos 8 caracteres']
-  }
+    required: [true, "La contraseña es obligatoria"],
+    minlength: [8, "La contraseña debe tener al menos 8 caracteres"],
+  },
 }, {
-  timestamps: true 
-})
+  timestamps: true,
+});
 
-
-userSchema.set('toJSON', {
+userSchema.set("toJSON", {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-    delete returnedObject.passwordHash
-  }
-})
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    delete returnedObject.passwordHash;
+  },
+});
 
-const User = mongoose.model('User', userSchema)
+const User = userConnection.model("User", userSchema);
 
-module.exports = User
+module.exports = User;
