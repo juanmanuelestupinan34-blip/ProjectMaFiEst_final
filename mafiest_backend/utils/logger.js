aef37d7a468a7edc8b0194269bf6a3f1fadfd62c
Juan-E-Medina <estupinanj89@gmail.com>
2025-08-16 13:56:46 -0500

    Update logger.js

--- mafiest_backend/utils/logger.js
+++ mafiest_backend/utils/logger.js
@@ -1,1 +1,16 @@
+const info = (dbName, ...params) => {
+  if (process.env.NODE_ENV !== 'test') {
+    console.log(`[${dbName}]`, ...params)
+  }
+}
+
+const error = (dbName, ...params) => {
+  if (process.env.NODE_ENV !== 'test') {
+    console.error(`[${dbName}]`, ...params)
+  }
+}
+
+module.exports = {
+  info, error
+}
 
