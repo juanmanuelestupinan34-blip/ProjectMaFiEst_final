7af8d577e5357a8f9d007d2c2013d9a15f806c02
Juan-E-Medina <estupinanj89@gmail.com>
2025-08-17 13:59:22 -0500

    Update login.js

--- mafiest_backend/controllers/login.js
+++ mafiest_backend/controllers/login.js
@@ -1,9 +1,40 @@
-import axios from 'axios'
-const baseUrl = '/api/login'
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const loginRouter = require('express').Router()
+const User = require('../models/user')
 
-const login = async credentials => {
-  const response = await axios.post(baseUrl, credentials)
-  return response.data
-}
+loginRouter.post('/', async (req, res, next) => {
+  try {
+    const { username, password } = req.body
+    if (!username || !password) {
+      return res.status(400).json({ error: 'Se requieren username y contraseña' })
+    }
 
-export default { login }
+    const user = await User.findOne({ username })
+    const passwordCorrect = user && await bcrypt.compare(password, user.passwordHash)
+
+    if (!user || !passwordCorrect) {
+      return res.status(401).json({ error: 'Usuario o contraseña incorrectos' })
+    }
+
+    const userForToken = {
+      id: user._id,
+      username: user.username,
+      rol: user.rol,
+    }
+
+    const token = jwt.sign(userForToken, process.env.SECRET, { expiresIn: 60 * 60 })
+
+    res.status(200).json({
+      token,
+      username: user.username,
+      name: user.name,
+      email: user.email,
+      rol: user.rol
+    })
+  } catch (error) {
+    next(error)
+  }
+})
+
+module.exports = loginRouter
