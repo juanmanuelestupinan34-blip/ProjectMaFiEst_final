c46eaf210536a94ddc2e7f2130249ffcb8b03e25
Juan-E-Medina <estupinanj89@gmail.com>
2025-08-17 20:35:50 -0500

    Create advisories.js

---
+++ client/src/services/advisories.js
@@ -0,0 +1,7 @@
+import axios from 'axios'
+const baseUrl = '/api/advisories'  
+
+const getAll = () => axios.get(baseUrl).then(res => res.data)
+const create = newObject => axios.post(baseUrl, newObject).then(res => res.data)
+
+export default { getAll, create }
