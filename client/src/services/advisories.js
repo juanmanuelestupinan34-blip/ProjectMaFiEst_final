619e0cd9f31dcc264ccaeb80cf853b2bc8b0dd38
Juan-E-Medina <estupinanj89@gmail.com>
2025-08-17 20:36:08 -0500

    Update advisories.js

--- client/src/services/advisories.js
+++ client/src/services/advisories.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = '/api/advisories'  
+const baseUrl = '/api/advisories'
 
 const getAll = () => axios.get(baseUrl).then(res => res.data)
 const create = newObject => axios.post(baseUrl, newObject).then(res => res.data)
