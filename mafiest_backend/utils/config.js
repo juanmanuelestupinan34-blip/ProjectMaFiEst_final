2b6d7f20beef26a92f2756155bbad400d32b5cf1
Juan-E-Medina <estupinanj89@gmail.com>
2025-08-18 20:19:45 -0500

    Update config.js

--- mafiest_backend/utils/config.js
+++ mafiest_backend/utils/config.js
@@ -2,14 +2,13 @@ require('dotenv').config()
 
 const PORT = process.env.PORT
 const SECRET = process.env.SECRET
+const MONGODB_URI = process.env.NODE_ENV === 'test'
+  ? process.env.TEST_MONGODB_URI
+  : process.env.MONGODB_URI
 
-// Solo UNA URI (la base general)
-const MONGO_URI = process.env.NODE_ENV === 'test'
-  ? process.env.TEST_MONGO_URI
-  : process.env.MONGO_URI
 
 module.exports = {
+  MONGODB_URI,
   PORT,
-  SECRET,
-  MONGO_URI
+  SECRET
 }
