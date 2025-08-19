da4abd6d75c1236867d5be069b71ad9d51f48876
Juan-E-Medina <estupinanj89@gmail.com>
2025-08-18 20:05:07 -0500

    Update app.js

--- mafiest_backend/app.js
+++ mafiest_backend/app.js
@@ -1,50 +1,33 @@
-const config = require('./utils/config')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const usersRouter = require('./controllers/users')
-const loginRouter = require('./controllers/login')
-const contactsRouter = require('./controllers/contacts')  
-const advisoriesRouter = require('./controllers/advisories') 
-const middleware = require('./utils/middleware')
-const logger = require('./utils/logger')
-const { userConnection, contactConnection, advisoryConnection } = require('./utils/db')
+const express = require("express")
+const cors = require("cors")
+const config = require("./utils/config")
+const connectDB = require("./utils/db")
 
+const usersRouter = require("./controllers/users")
+const loginRouter = require("./controllers/login")
+const contactsRouter = require("./controllers/contacts")
+const advisoriesRouter = require("./controllers/advisories")
+const middleware = require("./utils/middleware")
+const logger = require("./utils/logger")
+
+const app = express()
+
+// Conexión a la base de datos
+connectDB()
+
+// Middlewares
 app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
 app.use(middleware.tokenExtractor)
 
-// Conectar a la base de usuarios
-userConnection.once('open', () => {
-  logger.info(userConnection, 'Conexión establecida con la base de usuarios')
-})
-userConnection.on('error', (error) => {
-  logger.error(userConnection, 'Error en la conexión de usuarios:', error)
-})
-
-// Conectar a la base de contactos
-contactConnection.once('open', () => {
-  logger.info(contactConnection, 'Conexión establecida con la base de contactanos')
-})
-contactConnection.on('error', (error) => {
-  logger.error(contactConnection, 'Error en la conexión de contactanos:', error)
-})
-
-// Conectar a la base de asesorias
-advisoryConnection.once('open', () => {
-  logger.info(advisoryConnection, 'Conexión establecida con la base de asesorias')
-})
-advisoryConnection.on('error', (error) => {
-  logger.error(advisoryConnection, 'Error en la conexión de asesorias:', error)
-})
-
 // Rutas
-app.use('/api/users', usersRouter) 
-app.use('/api/login', loginRouter) 
-app.use('/api/contacts', contactsRouter)   
-app.use('/api/advisories', advisoriesRouter) 
+app.use("/api/users", usersRouter)
+app.use("/api/login", loginRouter)
+app.use("/api/contacts", contactsRouter)
+app.use("/api/advisories", advisoriesRouter)
 
+// Manejo de errores
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
