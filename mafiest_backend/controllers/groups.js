ad073f026ca6933355f8633f0af6869f5cd7c5a9
Juan-E-Medina <estupinanj89@gmail.com>
2025-08-31 18:43:45 -0500

    Update groups.js

--- mafiest_backend/controllers/groups.js
+++ mafiest_backend/controllers/groups.js
@@ -1,11 +1,1 @@
-const Institution = require('../models/institution');
 
-exports.createInstitution = async (req, res) => {
-  try {
-    const institution = new Institution(req.body);
-    await institution.save();
-    res.status(201).json(institution);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-};
