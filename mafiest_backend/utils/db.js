99e192266adf75592bbbb834d6ea09c93c5dd4b8
Juan-E-Medina <estupinanj89@gmail.com>
2025-08-31 21:39:22 -0500

    Update db.js

--- mafiest_backend/utils/db.js
+++ mafiest_backend/utils/db.js
@@ -1,7 +1,6 @@
 const { Sequelize } = require("sequelize");
 const config = require("./config");
 
-// Crear instancia de Sequelize
 const sequelize = new Sequelize(
   config.DB_NAME,
   config.DB_USER,
@@ -9,13 +8,8 @@ const sequelize = new Sequelize(
   {
     host: config.DB_HOST,
     dialect: config.DB_DIALECT,
-    logging: false, // cambia a true si quieres ver las queries en consola
+    logging: false,
   }
 );
-
-// Probar conexión
-sequelize.authenticate()
-  .then(() => console.log("✅ Conexión establecida con MySQL"))
-  .catch(err => console.error("❌ Error de conexión con MySQL:", err));
 
 module.exports = sequelize;
