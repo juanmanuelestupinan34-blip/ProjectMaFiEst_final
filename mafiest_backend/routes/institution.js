f39b158267ad0c0d53b9b8c3b7671853806bd2d2
Juan-E-Medina <estupinanj89@gmail.com>
2025-08-31 21:04:00 -0500

    Update and rename institution.js to groups.js

--- mafiest_backend/routes/institution.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const institutionCtrl = require('../controllers/institution');
-
-router.post('/', institutionCtrl.createInstitution);
-
-module.exports = router;
