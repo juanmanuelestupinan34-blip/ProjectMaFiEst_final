f4fe5ab55fb797d231c0eca6c18f5434f6769df1
Juan-E-Medina <estupinanj89@gmail.com>
2025-09-06 02:56:11 -0500

    Update ManageGroups.jsx

--- MaFiEst/mafiest_frontend/src/pages/admin/ManageGroups.jsx
+++ MaFiEst/mafiest_frontend/src/pages/admin/ManageGroups.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import '../styles/roles/admin.css'; 
 
 const ManageGroups = () => {
     const [groups, setGroups] = useState([]);
