9a9b373d14b3341b6000eb75558c0b6c264ee58e
Juan-E-Medina <estupinanj89@gmail.com>
2025-09-06 12:42:37 -0500

    Update Courses.jsx

--- MaFiEst/mafiest_frontend/src/pages/student/Courses.jsx
+++ MaFiEst/mafiest_frontend/src/pages/student/Courses.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import CourseCard from '../../components/CourseCard';
-import '../styles/roles/student.css';
+import '../styles/pages/courses.css';
 
 const Courses = () => {
     const [courses, setCourses] = useState([]);
