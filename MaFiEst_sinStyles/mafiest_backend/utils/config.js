9abdadbfedca971c9cbc24a537dd80348a935e7a
Juan-E-Medina <estupinanj89@gmail.com>
2025-09-06 07:28:38 +0000

    first

--- MaFiEst_sinStyles/mafiest_backend/utils/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    PORT: process.env.PORT || 3000,
-    DB_URL: process.env.DB_URL || 'postgres://user:password@localhost:5432/mafiest',
-    JWT_SECRET: process.env.JWT_SECRET || 'your_jwt_secret',
-    JWT_EXPIRATION: process.env.JWT_EXPIRATION || '1h',
-    LOG_LEVEL: process.env.LOG_LEVEL || 'info',
-};
