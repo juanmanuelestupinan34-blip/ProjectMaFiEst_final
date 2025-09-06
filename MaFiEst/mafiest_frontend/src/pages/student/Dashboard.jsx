3647954d106a0b7edf050aeefce60a7d5a276130
Juan-E-Medina <estupinanj89@gmail.com>
2025-09-06 12:41:42 -0500

    Update Dashboard.jsx

--- MaFiEst/mafiest_frontend/src/pages/student/Dashboard.jsx
+++ MaFiEst/mafiest_frontend/src/pages/student/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../styles/roles/student.css';
+import '../styles/pages/dashboard.css';
 
 const Dashboard = () => {
     return (
