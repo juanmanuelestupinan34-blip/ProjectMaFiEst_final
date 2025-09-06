ed2d79269b19048375c5626cac3d38ecc00f6cc1
Juan-E-Medina <estupinanj89@gmail.com>
2025-09-06 02:40:57 -0500

    Update Navbar.jsx

--- MaFiEst/mafiest_frontend/src/components/Navbar.jsx
+++ MaFiEst/mafiest_frontend/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './navbar.css';
+import '.. /styles/components/navbar.css';
 
 const Navbar = ({ userRole, onLogout }) => {
     return (
