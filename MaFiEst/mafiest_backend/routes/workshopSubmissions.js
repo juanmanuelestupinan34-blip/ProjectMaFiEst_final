615a275c63ceb17b063c470b122b905354670833
Juan-E-Medina <estupinanj89@gmail.com>
2025-09-08 18:26:35 -0500

    Update workshopSubmissions.js

--- MaFiEst/mafiest_backend/routes/workshopSubmissions.js
+++ MaFiEst/mafiest_backend/routes/workshopSubmissions.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const workshopSubmissionController = require('../controllers/workshopSubmissions');
+const { tokenExtractor, userExtractor } = require('../utils/middleware');
+const { isStudent } = require('../utils/roles');
 
-// CRUD entregas de talleres
-router.get('/', workshopSubmissionController.getWorkshopSubmissions);
-router.get('/:id', workshopSubmissionController.getWorkshopSubmissionById);
-router.post('/', workshopSubmissionController.createWorkshopSubmission);
-router.put('/:id', workshopSubmissionController.updateWorkshopSubmission);
-router.delete('/:id', workshopSubmissionController.deleteWorkshopSubmission);
+// Ver envíos de talleres → estudiante ve los suyos, admin podría auditar
+router.get('/', tokenExtractor, userExtractor, isStudent, workshopSubmissionController.getWorkshopSubmissions);
+router.get('/:id', tokenExtractor, userExtractor, isStudent, workshopSubmissionController.getWorkshopSubmissionById);
+
+// Crear envío de taller → solo estudiantes
+router.post('/', tokenExtractor, userExtractor, isStudent, workshopSubmissionController.createWorkshopSubmission);
+
+// Actualizar envío de taller → solo estudiante dueño
+router.put('/:id', tokenExtractor, userExtractor, isStudent, workshopSubmissionController.updateWorkshopSubmission);
+
+// Eliminar envío de taller → solo estudiante dueño
+router.delete('/:id', tokenExtractor, userExtractor, isStudent, workshopSubmissionController.deleteWorkshopSubmission);
 
 module.exports = router;
+
