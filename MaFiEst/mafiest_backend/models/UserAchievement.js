a4632b62d4c9eb4a2b5cc18b68d7b41b23992689
Juan-E-Medina <estupinanj89@gmail.com>
2025-09-08 13:46:55 -0500

    Update UserAchievement.js

--- MaFiEst/mafiest_backend/models/UserAchievement.js
+++ MaFiEst/mafiest_backend/models/UserAchievement.js
@@ -1,40 +1,35 @@
-// Modelo que relaciona usuarios con logros
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../utils/db');
+const { DataTypes } = require("sequelize");
+const sequelize = require("../utils/db");
 
-class UserAchievement extends Model {}
-
-UserAchievement.init({
-    id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-    },
-    userId: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-            model: 'Users',
-            key: 'id'
-        }
-    },
-    achievementId: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-            model: 'Achievements',
-            key: 'id'
-        }
-    },
-    dateEarned: {
-        type: DataTypes.DATE,
-        defaultValue: DataTypes.NOW
+const UserAchievement = sequelize.define("UserAchievement", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: "Users",
+      key: "id"
     }
+  },
+  achievementId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: "Achievements",
+      key: "id"
+    }
+  },
+  dateEarned: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  }
 }, {
-    sequelize,
-    modelName: 'UserAchievement',
-    tableName: 'UserAchievements',
-    timestamps: false
+  tableName: "UserAchievements",
+  timestamps: false
 });
 
 module.exports = UserAchievement;
