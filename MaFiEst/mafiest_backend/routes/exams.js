a578d294eb978a91deadfe3c61865b4ae4ff9541
Juan-E-Medina <estupinanj89@gmail.com>
2025-09-08 15:45:00 -0500

    Update exams.js

--- MaFiEst/mafiest_backend/routes/exams.js
+++ MaFiEst/mafiest_backend/routes/exams.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const examController = require('../controllers/exams');
 
+// CRUD examenes
+router.get('/', examController.getExams);
+router.get('/:id', examController.getExamById);
+router.post('/', examController.createExam);
+router.put('/:id', examController.updateExam);
+router.delete('/:id', examController.deleteExam);
+
+module.exports = router;
