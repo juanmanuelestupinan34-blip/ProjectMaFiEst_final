348af86714e6e41a97c1696a84b34ef3cb485fea
Juan-E-Medina <estupinanj89@gmail.com>
2025-09-08 18:12:15 -0500

    Update exams.js

--- MaFiEst/mafiest_backend/routes/exams.js
+++ MaFiEst/mafiest_backend/routes/exams.js
@@ -2,20 +2,13 @@ const express = require('express');
 const router = express.Router();
 const examController = require('../controllers/exams');
 const { tokenExtractor, userExtractor } = require('../utils/middleware');
+const { isTeacher } = require('../utils/roles');
 
-// Solo docentes pueden crear, actualizar y borrar exámenes
-const isTeacher = (req, res, next) => {
-  if (!req.user || req.user.role !== 'docente') {
-    return res.status(403).json({ error: 'Acceso denegado: solo docentes' });
-  }
-  next();
-};
-
-// Todos pueden ver exámenes (o solo estudiantes, si prefieres)
+// Ver exámenes (todos pueden ver, según el grupo)
 router.get('/', tokenExtractor, userExtractor, examController.getExams);
 router.get('/:id', tokenExtractor, userExtractor, examController.getExamById);
 
-// Solo docentes
+// Solo docentes pueden crear, actualizar y borrar
 router.post('/', tokenExtractor, userExtractor, isTeacher, examController.createExam);
 router.put('/:id', tokenExtractor, userExtractor, isTeacher, examController.updateExam);
 router.delete('/:id', tokenExtractor, userExtractor, isTeacher, examController.deleteExam);
