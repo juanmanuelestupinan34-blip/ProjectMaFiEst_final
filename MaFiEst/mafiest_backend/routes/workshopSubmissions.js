cec3589659509d6683c28c52f034b3a77cec5d44
Juan-E-Medina <estupinanj89@gmail.com>
2025-09-08 15:47:10 -0500

    Update workshopSubmissions.js

--- MaFiEst/mafiest_backend/routes/workshopSubmissions.js
+++ MaFiEst/mafiest_backend/routes/workshopSubmissions.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const workshopSubmissionController = require('../controllers/workshopSubmissions');
 
+// CRUD entregas de talleres
+router.get('/', workshopSubmissionController.getWorkshopSubmissions);
+router.get('/:id', workshopSubmissionController.getWorkshopSubmissionById);
+router.post('/', workshopSubmissionController.createWorkshopSubmission);
+router.put('/:id', workshopSubmissionController.updateWorkshopSubmission);
+router.delete('/:id', workshopSubmissionController.deleteWorkshopSubmission);
+
+module.exports = router;
