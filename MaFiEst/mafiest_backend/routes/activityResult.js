cab3cdebc1ab92f4800da4b66159bd146aaacfb4
juanmanuelestupinan34-blip <juanmanuelestupinan34@gmail.com>
2025-09-09 05:20:25 -0500

    Update activityResult.js

--- MaFiEst/mafiest_backend/routes/activityResult.js
+++ MaFiEst/mafiest_backend/routes/activityResult.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const activityResultController = require("../controllers/activityResultController");
+const { isTeacher } = require('../utils/roles');
+
+
 
 const router = express.Router();
 
-router.get("/", activityResultController.getActivityResults);
-router.get("/:id", activityResultController.getActivityResultById);
-router.post("/", activityResultController.createActivityResult);
-router.put("/:id", activityResultController.updateActivityResult);
-router.delete("/:id", activityResultController.deleteActivityResult);
+router.get("/", tokenExtractor, userExtractor, isTeacher, activityResultController.getActivityResults);
+router.get("/:id", tokenExtractor, userExtractor, isTeacher activityResultController.getActivityResultById);
+router.post("/", tokenExtractor, userExtractor, isTeacher activityResultController.createActivityResult);
+router.put("/:id", tokenExtractor, userExtractor, isTeacher activityResultController.updateActivityResult);
+router.delete("/:id", tokenExtractor, userExtractor, isTeacher activityResultController.deleteActivityResult);
 
 module.exports = router;
