23982329685811d7daba117623aec6b2b5931df9
juanmanuelestupinan34-blip <juanmanuelestupinan34@gmail.com>
2025-09-09 05:24:46 -0500

    Update activities.js

--- MaFiEst/mafiest_backend/routes/activities.js
+++ MaFiEst/mafiest_backend/routes/activities.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const activityController = require("../controllers/activityController");
+const { isTeacher } = require('../utils/roles');
 
 const router = express.Router();
 
-router.get("/", activityController.getActivities);
-router.get("/:id", activityController.getActivityById);
-router.post("/", activityController.createActivity);
-router.put("/:id", activityController.updateActivity);
-router.delete("/:id", activityController.deleteActivity);
+router.get("/", tokenExtractor, userExtractor, isTeacher, activityController.getActivities);
+router.get("/:id", tokenExtractor, userExtractor, isTeacher, activityController.getActivityById);
+router.post("/", tokenExtractor, userExtractor, isTeacher, activityController.createActivity);
+router.put("/:id", tokenExtractor, userExtractor, isTeacher, activityController.updateActivity);
+router.delete("/:id", tokenExtractor, userExtractor, isTeacher, activityController.deleteActivity);
 
 module.exports = router;
