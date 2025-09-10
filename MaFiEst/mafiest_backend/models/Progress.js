8e9ef22e632ba6a6b5359faa992d4d64646670e1
juanmanuelestupinan34-blip <juanmanuelestupinan34@gmail.com>
2025-09-09 19:59:47 -0500

    Update Progress.js

--- MaFiEst/mafiest_backend/models/Progress.js
+++ MaFiEst/mafiest_backend/models/Progress.js
@@ -1,26 +1,31 @@
-const { DataTypes } = require('sequelize');
+const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../utils/db');
 
-const Progress = sequelize.define('Progress', {
-    id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
+class Progress extends Model {}
+
+Progress.init({
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'Users',
+      key: 'id',
     },
-    userId: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-            model: 'Users',
-            key: 'id',
-        },
-    },
-    progress: {
-        type: DataTypes.JSONB,
-        allowNull: false,
-    },
+  },
+  progress: {
+    type: DataTypes.JSONB,
+    allowNull: false,
+  },
 }, {
-    timestamps: true,
+  sequelize,
+  modelName: 'Progress',
+  tableName: 'Progresses',
+  timestamps: true,
 });
 
 module.exports = Progress;
