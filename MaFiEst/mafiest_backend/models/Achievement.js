d0f02790a3ba54e2775d516f1bc94663187db8f2
juanmanuelestupinan34-blip <juanmanuelestupinan34@gmail.com>
2025-09-09 19:50:49 -0500

    Update Achievement.js

--- MaFiEst/mafiest_backend/models/Achievement.js
+++ MaFiEst/mafiest_backend/models/Achievement.js
@@ -1,34 +1,41 @@
-const { DataTypes } = require('sequelize');
+const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../utils/db');
 
-const Achievement = sequelize.define('Achievement', {
-    id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
+class Achievement extends Model {}
+
+Achievement.init({
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  title: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  icon: {
+    type: DataTypes.STRING(255),
+  },
+  type: {
+    type: DataTypes.STRING(50),
+    allowNull: false,
+    validate: {
+      isIn: [['progress', 'exam', 'constancy', 'participation']],
     },
-    title: {
-        type: DataTypes.STRING(100),
-        allowNull: false,
-    },
-    description: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-    },
-    icon: {
-        type: DataTypes.STRING(255),
-    },
-    type: {
-        type: DataTypes.STRING(50),
-        allowNull: false,
-        validate: {
-            isIn: [['progress', 'exam', 'constancy', 'participation']],
-        },
-    },
-    points: {
-        type: DataTypes.INTEGER,
-        defaultValue: 0,
-    },
+  },
+  points: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0,
+  },
+}, {
+  sequelize,
+  modelName: 'Achievement',
+  tableName: 'Achievements',
+  timestamps: true,
 });
 
 module.exports = Achievement;
